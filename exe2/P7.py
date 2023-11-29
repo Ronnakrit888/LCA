ab57c7fa3275136a9250a9db7c8f2c73194401d6
Ronnakrit888 <ronnakrit.t@kkumail.com>
2023-11-29 13:22:43 +0700

    edit P7

--- exe2/P7.py
+++ exe2/P7.py
@@ -10,10 +10,19 @@ def taylor_sin0(x, k):
 def taylor_sin_halfpi(x, k):
     result = 0
     for i in range(k+1):
-        result += ((-1) ** i) * (x - (math.pi / 2)) ** (2 * i + 1) / math.factorial(2 * i + 1)
+        result += ((-1) ** i) * (x - ( math.pi / 2)) ** (2 * i + 1) / math.factorial(2 * i + 1)
 
     return result
 
+def taylor_cos0(x, k):
+    result = 0
+    for i in range(k+1):
+        result += ((-1) ** i) * (x ** (2 * i)) / math.factorial(2 * i)
+    
+    return result
+
 pi = 3.141592653589793
+
 print(taylor_sin0(pi/4, 4))
 print(taylor_sin_halfpi(pi/4, 4))
+print(taylor_cos0(pi/4, 4))
